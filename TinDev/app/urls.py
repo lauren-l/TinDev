c46c43200cf96f5d7b6785af9b0d60b4996d6b22
Christian Matthew <matthew@Christians-MacBook-Pro.local>
2022-11-07 11:16:19 -0500

    added url paths for signup and home views

--- TinDev/app/urls.py
+++ TinDev/app/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='index'),
+    # path('', views.HomeView.as_view(), name='index'),
+    path('home/', views.home),
+    path('signup_candidate', views.signup_candidate),
+    path('signup_recruiter', views.signup_recruiter)
+
 ]
--- TinDev/app/views.py
+++ TinDev/app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.http import HttpResponse
 
 # Create your views here.
-class HomeView(ListView):
-    template_name = 'index.html'
+def home(request):
+    return render(request, 'index.html')
+
+def signup_candidate(request):
+    return render(request, 'app/signup_candidate.html')
+
+def signup_recruiter(request):
+    return render(request, 'app/signup_recruiter.html')
