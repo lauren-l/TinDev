bf646cf8ddac60e4e256a1051a2cffe11778245c
Christian Matthew <matthew@dhcp-10-5-99-218.dhcp.nd.edu>
2022-11-15 01:57:01 -0500

    set login and logout functionality, need signup function to work

--- TinDev/app/urls.py
+++ TinDev/app/urls.py
@@ -6,8 +6,8 @@ app_name = 'app'
 urlpatterns = [
     # path('', views.HomeView.as_view(), name='index'),
     path('home/', views.home),
+    path('logout', views.logout_user),
     path('signup_candidate', views.signup_candidate),
     path('signup_recruiter', views.signup_recruiter),
-    path('recruiter-dashboard', views.dashboard_recruiter)
-
+    path('recruiter_dashboard', views.dashboard_recruiter)
 ]
